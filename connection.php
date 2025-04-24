b6486138ae8277fefd105a0ce1ab93f583e2b06f
hibaanadani <hibaanadani1@gmail.com>
2025-04-24 15:38:05 +0300

    added connection.php

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "quizdatabase";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; // For testing the connection
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
